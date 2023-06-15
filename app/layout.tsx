bba17c378149d185333b5b2a6bc0da3e81f68709
thanhtu <vithanhtu21>
2023-06-15 19:13:05 +0700

    setup project

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import "./globals.css";
+import { Spectral } from "next/font/google";
+
+const spectral = Spectral({
+  weight: ["400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: "Liquor store app",
+  description: "Liquor store using nextjs",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={spectral.className}>{children}</body>
+    </html>
+  );
+}
