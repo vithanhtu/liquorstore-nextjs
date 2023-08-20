c445b4cc1012b80e1dbc38f47a3b3fe01fcd6c14
thanhtu <vithanhtu21>
2023-08-20 18:16:31 +0700

    config port 3000

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -19,7 +19,7 @@ const Menu = () => {
 
   useEffect(() => {
     axios
-      .get("http://localhost:3000/api/products")
+      .get(`http://localhost:${process.env.PORT || 3000}/api/products`)
       .then((response) => {
         const data = response.data;
         if (data.products) {
