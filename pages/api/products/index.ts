d6b7e275e7839f1dd61adc3aa86af78ebf80c9ab
thanhtu <vithanhtu21>
2023-08-21 18:16:31 +0700

    fix method

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -21,7 +21,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 export default async function handler(req: any, res: any) {
-  if (req.method === "GET") {
+  if (req.method === "GET" || "get") {
     try {
       const products = await prisma.product.findMany();
       return res.status(200).json({ products });
